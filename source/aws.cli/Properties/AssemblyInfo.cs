0104f10058e9415cc4c0aa09aa93c5232aac35f6
bounav <b.fourmond@gmail.com>
2017-04-19 15:16:31 +0100

    Version bumped to 0.1.3.0

--- source/aws.cli/Properties/AssemblyInfo.cs
+++ source/aws.cli/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Comsec")]
 [assembly: AssemblyProduct("aws.cli")]
-[assembly: AssemblyCopyright("Copyright © Comsec Investigations Ltd 2015")]
+[assembly: AssemblyCopyright("Copyright © Comsec Investigations Ltd 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4.0")]
+[assembly: AssemblyFileVersion("0.1.4.0")]
